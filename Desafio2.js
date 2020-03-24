5a4ff535b96493b6fc23540b45f0c2e744d55397
diegomeruoca <diegomeruoca@gmail.com>
2020-03-24 20:57:18 -0300

    Atualizado 1º semestrede 2020

---
+++ Desafio2.js
@@ -0,0 +1,35 @@
+$("#btSoma").click(function(){//Cria uma função caso o usuario clique no botão
+	let n1=parseInt($("#num1").val());//Atribui o valor do input na variavel
+	let n2=parseInt($("#num2").val());//Atribui o valor do input na variavel
+	let result=(n1+n2);//Calcula a soma
+	resultado(result);
+});
+$("#btSub").click(function(){//Cria uma função caso o usuario clique no botão
+	let n1=parseInt($("#num1").val());//Atribui o valor do input na variavel
+	let n2=parseInt($("#num2").val());//Atribui o valor do input na variavel
+	let result=(n1-n2);//Calcula 
+	resultado(result);
+});
+$("#btMult").click(function(){//Cria uma função caso o usuario clique no botão
+	let n1=parseInt($("#num1").val());//Atribui o valor do input na variavel
+	let n2=parseInt($("#num2").val());//Atribui o valor do input na variavel
+	let result=(n1*n2);//Calcula 
+	resultado(result);
+});
+$("#btDiv").click(function(){//Cria uma função caso o usuario clique no botão
+	let n1=parseInt($("#num1").val());//Atribui o valor do input na variavel
+	let n2=parseInt($("#num2").val());//Atribui o valor do input na variavel
+	if(n2==0){
+		$("#resultado").hide();
+		$("#erro").fadeIn(1200);
+	}else{
+		let result=(n1/n2);//Calcula
+		resultado(result);
+	}
+});
+function resultado(result){
+	$("#erro").hide();
+	$("#resultado").hide();
+	$("#resultado").text("Resultado da conta: "+result+"!!!");//Exibe o resultado
+	$("#resultado").fadeIn(1200);
+};
